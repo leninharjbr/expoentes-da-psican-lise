7b7c7c163754649fea8aaa69b9a2c1af9b3509ac
Marlene A T R Ferreira <158548020+leninharjbr@users.noreply.github.com>
2024-09-07 21:52:18 -0300

    Create dados.js

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Sigmund Freud",
+        descricao: "Considerado o fundador da psicanálise, Freud desenvolveu teorias sobre a estrutura da mente, a importância da sexualidade no desenvolvimento psicossexual, os mecanismos de defesa, a interpretação dos sonhos e a análise da transferência. Suas ideias revolucionaram a compreensão da mente humana e influenciaram diversas áreas do conhecimento.",
+        link: "https://pt.wikipedia.org/wiki/Sigmund_Freud",
+        tags: "psicanálise fundador sexualidade sonhos"
+    },
+    {
+        titulo: "Carl Gustav Jung:",
+        descricao: "Discípulo de Freud, Jung divergiu de seu mestre em alguns pontos e desenvolveu a psicologia analítica. Jung enfatizou a importância dos arquétipos, do inconsciente coletivo e da espiritualidade na psique humana. Seus conceitos como introversão e extroversão se tornaram amplamente utilizados na psicologia..",
+        link: "https://pt.wikipedia.org/wiki/Carl_Gustav_Jung",
+        tags: "discípulo coletivo conceito conceitos analítica"
+    },
+    {
+        titulo: "Jacques Lacan",
+        descricao: "Psicanalista francês, Lacan reformulou as teorias freudianas utilizando conceitos da linguística e da filosofia. Sua obra é complexa e abstrata, mas exerceu grande influência sobre a psicanálise contemporânea, especialmente na França. Lacan enfatizou a importância da linguagem na constituição do sujeito e a relação entre o desejo e a falta.",
+        link: "https://pt.wikipedia.org/wiki/Jacques_Lacan",
+        tags: "linguistica linguagem constituição sujeito desejo"
+    }
+];
